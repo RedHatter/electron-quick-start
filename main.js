5197616f169562d341e2ca6a4547fc0273639899
Timothy Johnson <timothy@idioticdev.com>
2017-03-07 17:30:45 -0800

    Change icon.

--- main.js
+++ main.js
@@ -15,8 +15,6 @@ function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
-  console.log(path.join(__dirname, 'icon.png'))
-
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
